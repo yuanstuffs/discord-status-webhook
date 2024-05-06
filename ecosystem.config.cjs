7ee91129768ea8b2eec70137995b2058c098d9af
yuansheng1549 <yuansheng1515@icloud.com>
2024-05-06 15:32:13 +0800

    chore: i need therapy
    permalink: http://whatthecommit.com/4e02be5198b28f786edc13025f0e3366

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
 	apps: [
 		{
 			name: 'discord-status-webhook',
-			script: 'src/index.ts',
+			script: 'src/index.ts', // script is required but it's not in used.
 			interpreter: 'yarn',
 			interpreter_args: 'start:tsx'
 		}
