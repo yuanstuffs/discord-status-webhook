93be95dc1eba590a3ace543314e8fe85992ba785
yuansheng1549 <yuansheng1515@icloud.com>
2024-05-16 22:25:01 +0800

    chore(EmbedColor): lowercase

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,9 @@
 export const API_BASE = 'https://srhpyqt94yxb.statuspage.io/api/v2/';
 
 export enum EmbedColor {
-	GREEN = '#43b581',
-	YELLOW = '#faa61a',
-	ORANGE = '#f26522',
-	RED = '#f04747',
-	BLACK = '#737f8d'
+	Green = '#43b581',
+	Yellow = '#faa61a',
+	Orange = '#f26522',
+	Red = '#f04747',
+	Black = '#737f8d'
 }
